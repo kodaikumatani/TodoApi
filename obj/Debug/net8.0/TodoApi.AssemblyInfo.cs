d1146368a16bba8d12e7c300e886f0feeb0c344b
kodaikumatani <koudai.aco5423@gmail.com>
2024-06-08 02:18:31 +0900

    Swagger を使って API テスト UI を作成

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9aa61de89ae9c74fa9e5ba7ab75c6e4af2ea0ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31060df5a1f35716b685e580cba0337e1d496745")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
